d6fc06f41a0e5e1c6fb2a71eba980546cc7110f4
Hyago Oliveira <hyagogow@gmail.com>
2020-10-25 11:09:03 -0300

    Add CinemachineFollowAttacher component

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -5,6 +5,8 @@ The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
 ## [Unreleased]
+### Added
+- CinemachineFollowAttacher component
 ### Changed
 - Improve First Region creation. Check if main camera is perspective or orthographic
 - Improve Create Regions GUI. Inspector and Overlay Scene show the same extra GUI helpers
---
+++ Runtime/CinemachineFollowAttacher.cs
@@ -0,0 +1,59 @@
+﻿using Cinemachine;
+using UnityEngine;
+
+namespace ActionCode.Cinemachine
+{
+    /// <summary>
+    /// Cinemachine Follow Attacher.
+    /// <para>It will attach a Transform to be followed by the local VirtualCamera.</para>
+    /// </summary>
+    [RequireComponent(typeof(CinemachineVirtualCamera))]
+    public sealed class CinemachineFollowAttacher : MonoBehaviour
+    {
+        [SerializeField, Tooltip("Virtual Camera to attach target Transform.")]
+        private CinemachineVirtualCamera virtualCamera;
+        [TagField, Tooltip("If set, it'll search and attach the first GameObject with this tag on Start function.")]
+        public string tagOnStart = string.Empty;
+
+        private void Reset()
+        {
+            virtualCamera = GetComponent<CinemachineVirtualCamera>();
+        }
+
+        private void Start()
+        {
+            Attach(tagOnStart);
+        }
+
+        /// <summary>
+        /// Attaches the given target GameObject to be followed by VirtualCamera.
+        /// </summary>
+        /// <param name="target">A GameObject to be followed.</param>
+        public void Attach(GameObject target)
+        {
+            if (target) Attach(target.transform);
+        }
+
+        /// <summary>
+        /// Attaches the given target to be followed by VirtualCamera.
+        /// </summary>
+        /// <param name="target">A Transform to be followed.</param>
+        public void Attach(Transform target)
+        {
+            virtualCamera.Follow = target;
+        }
+
+        /// <summary>
+        /// Attaches the first GameObject with the given tag to be followed by VirtualCamera.
+        /// </summary>
+        /// <param name="tag">A Tag to search for a GameObject.</param>
+        public void Attach(string tag)
+        {
+            var invalidTag = tag.Length == 0;
+            if (invalidTag) return;
+
+            var target = GameObject.FindWithTag(tagOnStart);
+            Attach(target);
+        }
+    }
+}
